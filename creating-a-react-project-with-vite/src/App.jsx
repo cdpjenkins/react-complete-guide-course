e7aeb14ea35036a795942050c9815ceec28bfc2a
Chris Jenkins <cdpjenkins@gmail.com>
2025-04-20 09:17:23 +0100

    Make it possible to both increment and decrement the counter

--- creating-a-react-project-with-vite/src/App.jsx
+++ creating-a-react-project-with-vite/src/App.jsx
@@ -19,11 +19,14 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Increment
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setCount((count) => count - 1)}>
+            Decrement
+        </button>
+        <div>
+            Count is {count}
+        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
