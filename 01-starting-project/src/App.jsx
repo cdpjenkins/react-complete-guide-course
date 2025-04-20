6bd273550a97f23cf39317d7d397895b7df5e520
Chris Jenkins <cdpjenkins@gmail.com>
2025-04-21 00:24:19 +0100

    Dynamically generate attributes + load images via React imports

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from "./assets/react-core-concepts.png";
+
 const adjectives = ["Fundamental", "Fun", "Awesome", "Core", "Super", "Crucial", "Exciting"];
 
 function getRandomInt(max) {
@@ -9,7 +11,7 @@ function Header() {
 
     return (
         <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+            <img src={reactImg} alt="Stylized atom"/>
             <h1>React Essentials</h1>
             <p>
                 {adjective} React concepts you will need for almost any app you are
