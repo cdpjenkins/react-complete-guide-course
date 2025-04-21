720cda8758ba2518625a39df2c4dcc56d559d078
Chris Jenkins <cdpjenkins@gmail.com>
2025-04-21 20:37:24 +0100

    Start adding a TabButton component

--- 03-react-essentials/src/App.jsx
+++ 03-react-essentials/src/App.jsx
@@ -2,6 +2,7 @@
 import {CORE_CONCEPTS} from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
+import TabButton from "./components/TabButton";
 
 function App() {
   return (
@@ -9,15 +10,24 @@ function App() {
       <Header/>
       <main>
         <section id="core-concepts">
-            <h2>Core Concepts</h2>
-            <ul>
-                <CoreConcept {...CORE_CONCEPTS[0]}/>
-                <CoreConcept {...CORE_CONCEPTS[1]}/>
-                <CoreConcept {...CORE_CONCEPTS[2]}/>
-                <CoreConcept {...CORE_CONCEPTS[3]}/>
-            </ul>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id="examples">
+          <h2>
+            <menu>
+              <TabButton>Components</TabButton>
+              <TabButton>JSX</TabButton>
+              <TabButton>Props</TabButton>
+              <TabButton>State</TabButton>
+            </menu>
+          </h2>
+        </section>
       </main>
     </div>
   );
