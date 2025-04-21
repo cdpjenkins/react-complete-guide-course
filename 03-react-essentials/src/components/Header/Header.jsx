6e68574c2326e53d5826ee269f63d1c0fcec2b21
Chris Jenkins <cdpjenkins@gmail.com>
2025-04-21 18:09:53 +0100

    Move each component, along with its css, into its own directory

--- 03-react-essentials/src/components/Header/Header.jsx
+++ 03-react-essentials/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import reactImg from "../assets/react-core-concepts.png";
+import reactImg from "../../assets/react-core-concepts.png";
+import "./Header.css"
 
 const adjectives = ["Fundamental", "Fun", "Awesome", "Core", "Super", "Crucial", "Exciting"];
 
