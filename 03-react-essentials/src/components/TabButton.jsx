458d762506329bac26c62bc648b0b33010d1a4b4
Chris Jenkins <cdpjenkins@gmail.com>
2025-04-21 21:32:42 +0100

    Add onClick handler to button

--- 03-react-essentials/src/components/TabButton.jsx
+++ 03-react-essentials/src/components/TabButton.jsx
@@ -1,5 +1,9 @@
 export default function TabButton({children}) {
+    function handleClick() {
+        console.log("Button was clicked!");
+    }
+
     return (
-        <li><button>{children}</button></li>
+        <li><button onClick={handleClick}>{children}</button></li>
     );
 }
